0308633e29ccc24a1a54ea3117d095f734fa3259
Kenneth Tran <quoc.tran@okstate.edu>
2018-03-30 12:00:36 -0500

    fix cors

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const url = "https://randomyoutube.net/api/getvid?api_token=wb8RC5sDl1rV2PagQI34pkhK6gGpYIn1xwI6WPbgFDJeJFLbvFPRBsJukX2S";
+const url = 'https://randomyoutube.net/api/getvid?api_token=wb8RC5sDl1rV2PagQI34pkhK6gGpYIn1xwI6WPbgFDJeJFLbvFPRBsJukX2S'
 
 const vm = new Vue({
         el: '#app',
         mounted() {
-          axios.get(url).then(response => {
+          axios.get(url,{ crossdomain: true })
+          .then(response => {
             this.results = response.data
+            console.log(response)
+          })
+          .catch(error =>{
+            console.log(error)
           })
         },
         data: {
           results: [],
+
         }
 
 
